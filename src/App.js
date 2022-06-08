53faec273b249d9f09ecebbb87f0c4e6b0b28ac1
vladimirjv <ivladimirjuarez@outlook.com>
2022-06-08 14:10:15 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
 
       <button
         type="button"
-        className=""
+        className="w-96 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         onClick={setNewTodo.bind(this, true)}
       >
         New Todo
@@ -105,7 +105,7 @@ function App() {
         />
       ) : null}
 
-      {!selectedTodo
+      {!newTodo && !selectedTodo
         ? todos.map((todo) => (
             <TodoCard
               key={todo.id}
