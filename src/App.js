5d462bc80e7acb99370543e8735a292ee3ae201d
vladimirjv <ivladimirjuarez@outlook.com>
2022-06-08 14:12:47 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -86,13 +86,15 @@ function App() {
 
       <TodoTabs selectedTab={todoType} onChangeTab={onChangeTab} tabs={tabs} />
 
-      <button
-        type="button"
-        className="w-96 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-        onClick={setNewTodo.bind(this, true)}
-      >
-        New Todo
-      </button>
+      {!newTodo ? (
+        <button
+          type="button"
+          className="w-96 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          onClick={setNewTodo.bind(this, true)}
+        >
+          New Todo
+        </button>
+      ) : null}
 
       {selectedTodo ? (
         <Todo
