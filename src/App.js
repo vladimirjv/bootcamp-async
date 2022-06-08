f674072a76546c33287f11ad19ab7548ea13c7e0
vladimirjv <ivladimirjuarez@outlook.com>
2022-06-08 14:14:53 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
   const onChangeTab = useCallback(
     (tab) => {
       setSelectedTodo(null);
+      setNewTodo(false);
       setTodoType(tab.key);
     },
     [todoType]
@@ -86,7 +87,7 @@ function App() {
 
       <TodoTabs selectedTab={todoType} onChangeTab={onChangeTab} tabs={tabs} />
 
-      {!newTodo ? (
+      {!newTodo && !selectedTodo ? (
         <button
           type="button"
           className="w-96 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
