931d813f5d7eb79a81c8b0f9d60a3949ed2ea5ae
vladimirjv <ivladimirjuarez@outlook.com>
2022-06-08 14:09:24 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -86,6 +86,14 @@ function App() {
 
       <TodoTabs selectedTab={todoType} onChangeTab={onChangeTab} tabs={tabs} />
 
+      <button
+        type="button"
+        className=""
+        onClick={setNewTodo.bind(this, true)}
+      >
+        New Todo
+      </button>
+
       {selectedTodo ? (
         <Todo
           todo={selectedTodo}
