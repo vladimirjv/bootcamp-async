528203402166b3921e3d8f1c791adef55e656666
vladimirjv <ivladimirjuarez@outlook.com>
2022-06-08 09:57:22 -0500

    1 file modified

--- src/api.js
+++ src/api.js
@@ -3,20 +3,15 @@ export const getURLTodos = (todoCompleted) => {
     ? 'https://jsonplaceholder.typicode.com/todos?completed=false'
     : 'https://jsonplaceholder.typicode.com/todos?completed=true';
 };
-
-// export const getTodos = async (todoCompleted) => {
-//   const url = getURLTodos(todoCompleted);
-//   const json = await fetch(url).then((response) => response.json());
-//   return todoCompleted ? json.slice(0, 5) : json.slice(5, 10);
-// };
-
 export const getTodos = async (URL, todoCompleted) => {
   const json = await fetch(URL).then((response) => response.json());
   return todoCompleted ? json.slice(0, 5) : json.slice(5, 10);
 };
 
-export const getTodoById = async (todoId) => {
-  const url = `https://jsonplaceholder.typicode.com/posts/${todoId}`;
-  const response = await fetch(url);
-  return await response.json();
-};
+export const deleteTodo = async (todoId) => {
+  const response = await fetch({
+    method: 'delete',
+    url: `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  })
+  return response;
+}
